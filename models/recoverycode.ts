a89904c429d4ccb545dd8f0c02dc59cfe8c9376e
htsuruo <tsuru.dev@gmail.com>
2025-09-21 16:34:33 +0900

    refactor: update database model definitions

--- models/recoverycode.ts
+++ models/recoverycode.ts
@@ -1,29 +1,43 @@
-import { Model, DataTypes, Sequelize } from 'sequelize';
+import { DataTypes, Model, Optional, Sequelize } from 'sequelize'
 
-interface RecoveryCodeAttributes {
-  user_id: string;
-  code_hash: string;
-  is_used: boolean;
+export interface RecoveryCodeAttributes {
+  id: number
+  user_id: string
+  code_hash: string
+  is_used: boolean
 }
+type RecoveryCodeCreationAttributes = Optional<RecoveryCodeAttributes, 'id'>
 
-class RecoveryCode extends Model<RecoveryCodeAttributes> implements RecoveryCodeAttributes {
-  public user_id!: string;
-  public code_hash!: string;
-  public is_used!: boolean;
+class RecoveryCode
+  extends Model<RecoveryCodeAttributes, RecoveryCodeCreationAttributes>
+  implements RecoveryCodeAttributes
+{
+  public id!: number
+  public user_id!: string
+  public code_hash!: string
+  public is_used!: boolean
 
   public static associate(models: any) {
-    RecoveryCode.belongsTo(models.User, { foreignKey: 'user_id' });
+    RecoveryCode.belongsTo(models.User, { foreignKey: 'user_id' })
   }
 }
 
 export default (sequelize: Sequelize) => {
-  RecoveryCode.init({
-    user_id: DataTypes.UUID,
-    code_hash: DataTypes.STRING,
-    is_used: DataTypes.BOOLEAN
-  }, {
-    sequelize,
-    modelName: 'RecoveryCode',
-  });
-  return RecoveryCode;
-};
+  RecoveryCode.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      user_id: DataTypes.UUID,
+      code_hash: DataTypes.STRING,
+      is_used: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: 'RecoveryCode',
+    }
+  )
+  return RecoveryCode
+}
